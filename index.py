54c9af95d44d907692288af8a7507a22c3827327
Krushil Naik <krushilnaik96@gmail.com>
2022-07-30 01:55:50 +0000

    import Flask

--- index.py
+++ index.py
@@ -1,6 +1,31 @@
+"""
+This is a simple website using the current directory as a server.
+The frontend is pretty simple;
+Enter some text and hit sumbit for an ASCII art representation.
+If you typed an unsupported character, it'll alert you.
+
+I chose Flask over Django because of substantially less boilerplate code,
+which is better for something being graded by a human
+
+Changes from midterm:
+    Use Flask instead of http.server
+    Revamp the site UI a bit
+
+To run:
+    1) `python KrushilNaikFinalProject.py` from 'ascii_flask' directory
+    2) Visit http://localhost:3000 from any browser
+"""
+
+import sys
 import json
 from models.Letter import Letter
-from util.custom_logging import debug
+from util.custom_logging import critical, debug
+
+try:
+    from flask import Flask, render_template, request, redirect, url_for, session
+except ImportError:
+    critical("Failed to import Flask. Did you install it with pip?")
+    sys.exit(1)
 
 app = Flask(__name__)
 app.secret_key = "SUPER_SECRET_KEY"
